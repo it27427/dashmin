6e488337db292323869931d8ec542d2b75735830
it27427 <istiak.tushar.qtec@gmail.com>
2023-03-09 17:56:18 +0600

    gprecord, printcard, information-booth completed

---
+++ js/tables/gprecordtable.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+//   $('#gprecord').DataTable({
+//     dom: 'Bfrtip',
+//     searching: false,
+//     paging: false,
+//     info: false,
+//   });
+// });
+
+// $(document).ready(function() {
+//   $('#gatepass').DataTable({
+//     dom: 'Bfrtip',
+//     searching: false,
+//     paging: false,
+//     info: false,
+//   });
+// });
